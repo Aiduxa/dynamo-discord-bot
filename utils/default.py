32b36ba6e41355ee435f8fd58aaf0b4db9bf2c5c
Marseel-E <74118311+Marseel-E@users.noreply.github.com>
2023-01-04 14:23:50 -0500

    Created Default.py in utils directory

--- utils/default.py
+++ utils/default.py
@@ -1,1 +1,9 @@
-#shit goes here that mr marseel wants 
+from dataclasses import dataclass
+
+from discord import Object as D_Objct
+
+
+@dataclass
+class Default:
+    SERVER: D_Object = D_Object(id=1060218266670346370)
+    PRIMARY_COLOR: int = int("1aff79", 16)
